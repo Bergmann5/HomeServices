ad95d14ebbb677c1eadc4280b820313767c0432a
Bergmann5 <talemwar@gmail.com>
2022-01-06 16:01:02 +0300

    livewire

---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+use App\Http\Livewire\Admin\AdminServicesByCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddServiceCategoryComponent;
+use App\Http\Livewire\Admin\AdminSliderComponent;
+use App\Http\Livewire\Admin\AdminAddSliderComponent;
+use App\Http\Livewire\Admin\AdminEditSliderComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\Admin\AdminEditServiceCategoryComponent;
+use App\Http\Livewire\Admin\AdminServiceCategoryComponent;
+use App\Http\Livewire\Admin\AdminServicesComponent;
+use App\Http\Livewire\Admin\AdminAddServiceComponent;
+use App\Http\Livewire\Admin\AdminEditServiceComponent;
+use App\Http\Livewire\Customer\CustomerDashboardComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ServicesByCategoryComponent;
+use App\Http\Livewire\ServiceCategoriesComponent;
+use App\Http\Livewire\ServiceDetailsComponent;
+use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
+Route::get('/',HomeComponent::class)->name('home');
+Route::get('/service_categories',ServiceCategoriesComponent::class)->name('home.service_cat');
+Route::get('/{category_slug}/services',ServicesByCategoryComponent::class)->name('home.services_by_category');
+Route::get('/service/{service_slug}',ServiceDetailsComponent::class)->name('home.service_details');
+
+//for Customer
+    Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+        Route::get('/customer/dashboard',CustomerDashboardComponent::class)->name('customer.dashboard');
+
+        });
+
+//for Service Provider
+    Route::middleware(['auth:sanctum', 'verified','authsprovider'])->group(function(){
+        Route::get('/sprovider/dashboard',SproviderDashboardComponent::class)->name('sprovider.dashboard');
+        });
+
+
+//for Admin
+    Route::middleware(['auth:sanctum', 'verified','authadmin'])->group(function(){
+    Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+    Route::get('/admin/service-categories',AdminServiceCategoryComponent::class)->name('admin.service_categories');
+    Route::get('/admin/service-category/add',AdminAddServiceCategoryComponent::class)->name('admin.add_service_cat');
+    Route::get('/admin/service-category/edit/{category_id}',AdminEditServiceCategoryComponent::class)->name('admin.edit_cat');
+    Route::get('/admin/all-services',AdminServicesComponent::class)->name('admin.all_services');
+    Route::get('/admin/{category_slug}/services',AdminServicesByCategoryComponent::class)->name('admin.services_by_category');
+    Route::get('/admin/service/add',AdminAddServiceComponent::class)->name('admin.add_service');
+    Route::get('/admin/service/edit/{service_slug}',AdminEditServiceComponent::class)->name('admin.edit_service');
+    Route::get('/admin/slider',AdminSliderComponent::class)->name('admin.slider');
+    Route::get('/admin/slider/add',AdminAddSliderComponent::class)->name('admin.add_slide');
+    Route::get('/admin/slider/edit/{slide_id}',AdminEditSliderComponent::class)->name('admin.edit_slide');
+    });
